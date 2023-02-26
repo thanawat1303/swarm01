83eee125f9f690512c6a457c3872794f90c0078d
thanawat1303 <fquarius13@gmail.com>
2023-02-26 23:31:49 +0700

    Edit container

--- app/index.php
+++ app/index.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>apache-php@19</title>
+</head>
+<body>
 <?php
-    echo "<center><h1>WELCOME</h1></center><center>SPCN19</center>"
+    echo "<center></center>"
 ?>
+    <center><h1>WELCOME</h1></center>
+    <center>SPCN19</center>
+</body>
+</html>
